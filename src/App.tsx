2d324e329460d347b9450cbed23f987ee4768fd8
Jomar Granada <jomart.granada@gmail.com>
2023-09-22 13:33:21 +0800

    added class folder

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { ThemeContextProvider } from './components/context/ThemeContext'
 import { Box } from './components/context/Box'
 import { UserContextProvider } from './components/context/UserContext'
 import { User } from './components/context/User'
+import { Counter } from './components/class/Counter'
 
 function App() {
   const personName= {
@@ -52,6 +53,7 @@ function App() {
       <UserContextProvider>
         <User />
       </UserContextProvider>
+      <Counter message='The count value is'/>
     </div>
   );
 }
